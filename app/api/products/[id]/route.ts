7a0bc4b03b6d8eecdd76d0abbe01766d6079cd27
aalokjha07 <aalokjha2018@gmail.com>
2026-05-11 17:11:01 +0530

    added dynamic api rendering

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import dbConnect from '@/lib/mongodb';
 import Product from '@/lib/models/Product';
+export const dynamic = 'force-dynamic';
 
 export async function GET(
   request: NextRequest, 
