5cf4c1f144e12867ddbcb73f1b3effe46e6a05f1
aalokjha07 <aalokjha2018@gmail.com>
2026-05-11 17:16:18 +0530

    removed the dburi handling

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  if (process.env.NODE_ENV === 'production') {
+    console.warn("Warning: MONGODB_URI is not defined.");
+  }
 }
 
 let cached = global as any;
